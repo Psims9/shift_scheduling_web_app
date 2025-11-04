5d4f31793cf55303a84aed86bf05f84f03d13db2
psims99 <psimenos99@gmail.com>
2025-11-04 16:36:36 +0200

    add features to create/edit/delete workers

--- shift_scheduling/scheduler/urls.py
+++ shift_scheduling/scheduler/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('workers/', views.WorkerListView.as_view(), name='workers'),
-    path('worker/<int:pk>', views.WorkerDetailView.as_view(), name='detail-worker'),    
+    path('worker/<int:pk>', views.WorkerDetailView.as_view(), name='detail-worker'),
+    path('worker/create/', views.WorkerCreate.as_view(), name='worker-create'),
+    path('worker/<int:pk>/update/', views.WorkerUpdate.as_view(), name='worker-update'),
+    path('worker/<int:pk>/delete/', views.WorkerDelete.as_view(), name='worker-delete'),    
 ]
--- shift_scheduling/scheduler/views.py
+++ shift_scheduling/scheduler/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Worker
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 def index(request):
     num_workers = Worker.objects.all().count()
@@ -9,6 +12,26 @@ def index(request):
 
 class WorkerListView(generic.ListView):
     model = Worker
+    paginate_by = 10
 
 class WorkerDetailView(generic.DetailView):
     model = Worker
+
+class WorkerCreate(CreateView):
+    model = Worker
+    fields = '__all__'
+
+class WorkerUpdate(UpdateView):
+    model = Worker
+    fields = '__all__'
+
+class WorkerDelete(DeleteView):
+    model = Worker
+    success_url = reverse_lazy('workers')
+
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+            return HttpResponseRedirect(self.success_url)
+        except:
+            return HttpResponseRedirect('worker-delete', kwargs={'pk': self.object.pk})
