b9c143bcfb2f5e58cadfd1dd52ac533eea855cff
psims99 <psimenos99@gmail.com>
2025-11-13 21:13:26 +0200

    add lexicographic optimization

--- shift_scheduling/scheduler/create_schedule.py
+++ shift_scheduling/scheduler/create_schedule.py
@@ -7,29 +7,8 @@ def create_schedule(schedule_period: datetime.date, employee_data: dict):
     model = cp_model.CpModel()
 
     num_days = calendar.monthrange(schedule_period.year, schedule_period.month)[1]
-
-    decision_vars = {e: {day: model.new_bool_var(f'employee_{e}_on_day_{day}') for day in range(1, num_days + 1)} for e in employee_data}
-
-    for day in range(1, num_days + 1):
-        model.add(sum(decision_vars[e][day] for e in employee_data) == 2)
-    
-    for e in employee_data:
-        if employee_data[e]['unavailable_dates']:
-            for date in employee_data[e]['unavailable_dates']:
-                day = datetime.datetime.strptime(date, "%Y-%m-%d").day
-                model.add(decision_vars[e][day] == 0)
-                
-    # employee_shift_count = {id: int model variable for employee's montlhy shift count}
-    employee_shift_count = {e: model.new_int_var(0, num_days, f'{e}_shift_count') for e in employee_data}
-
-    for e in employee_data:
-        model.add(employee_shift_count[e] == sum(decision_vars[e][day] for day in range(1, num_days + 1)))
-    
-    min_shifts = model.new_int_var(0, num_days, 'min_shifts')
-    model.add_min_equality(min_shifts, employee_shift_count.values())
-
-    max_shifts = model.new_int_var(0, num_days, 'max_shifts')
-    model.add_max_equality(max_shifts, employee_shift_count.values())
+    shift_interval = math.floor(num_days/math.ceil((num_days*2)/len(employee_data)))
+    shift_interval_violations = []
 
     month_cal = calendar.monthcalendar(schedule_period.year, schedule_period.month)
     weekends = []
@@ -38,50 +17,77 @@ def create_schedule(schedule_period: datetime.date, employee_data: dict):
             if i >= 5 and day != 0:
                 weekends.append(day)
 
-    employee_wknd_shift_count = {e: model.new_int_var(0, len(weekends), f'{e}_wknd_shift_count') for e in employee_data}
+    # decision variables
+    decision_vars = {}
     
-    for e in employee_data:
-        model.add(employee_wknd_shift_count[e] == sum(decision_vars[e][day] for day in weekends))
-    
+    # auxiliary variables
+    employee_shift_count = {}
+    employee_wknd_shift_count = {}
+    max_shifts = model.new_int_var(0, num_days, 'max_shifts')
+    min_shifts = model.new_int_var(0, num_days, 'min_shifts')
     min_wknd_shifts = model.new_int_var(0, len(weekends), 'min_wknd_shifts')
-    model.add_min_equality(min_wknd_shifts, employee_wknd_shift_count.values())
-
     max_wknd_shifts = model.new_int_var(0, len(weekends), 'max_wknd_shifts')
-    model.add_max_equality(max_wknd_shifts, employee_wknd_shift_count.values())
 
-    shift_interval = math.floor(num_days/math.ceil((num_days*2)/len(employee_data)))
+    for id in employee_data:
+        decision_vars[id] = {}
+        employee_shift_count[id] = model.new_int_var(0, num_days, f'{id}_shift_count')
+        employee_wknd_shift_count[id] = model.new_int_var(0, len(weekends), f'{id}_wknd_shift_count')
 
-    shift_interval_violations = []
+        for day in range(1, num_days + 1):
+            decision_vars[id][day] = model.new_bool_var(f'empl_{id}_on_{day}')
 
-    for e in employee_data:
-        # number of interval start days = num_days - shift_interval + 1 (we stop when the end day equals the end of the month).
+            if datetime.date(schedule_period.year, schedule_period.month, day).strftime("%Y-%m-%d") in employee_data[id]['unavailable_dates']:
+                model.add(decision_vars[id][day] == 0)
+            
+            if day != 1:
+                model.add(decision_vars[id][day - 1] + decision_vars[id][day] <= 1)
+        
+        model.add(employee_shift_count[id] == sum(decision_vars[id][day] for day in range(1, num_days + 1)))
+        model.add(employee_wknd_shift_count[id] == sum(decision_vars[id][day] for day in weekends))
+
         for interval_start in range(1, num_days - shift_interval + 2):
-            # the interval includes the starting day, so the interval end is = interval_start + interval - 1
             interval_end = interval_start + shift_interval - 1
 
-            # violation = 1 if the employee works more than 2 shifts in the given interval, 0 otherwise
-            violation = model.new_bool_var(f'too_many_shifts_for_{e}_from_{interval_start}_to_{interval_end}')
-
-            # force violation = 1 if employee works more than 2 shifts in the current interval
-            model.add(sum(decision_vars[e][day] for day in range(interval_start, interval_end + 1)) >= 2).only_enforce_if(violation)
-
-            # force violation = 0 if employee does not work more than 2 shifts in the current interval
-            model.add(sum(decision_vars[e][day] for day in range(interval_start, interval_end + 1)) < 2).only_enforce_if(violation.Not())
+            violation = model.new_bool_var(f'interval_violation_for_{id}_from_{interval_start}_to_{interval_end}')
+            
+            model.add(sum(decision_vars[id][day] for day in range(interval_start, interval_end + 1)) >= 2).only_enforce_if(violation)
+            model.add(sum(decision_vars[id][day] for day in range(interval_start, interval_end + 1)) < 2).only_enforce_if(violation.Not())
 
             shift_interval_violations.append(violation)
         
-    coef = [10, 3.5, 1.5]
-    obj_func = coef[0]*(max_shifts - min_shifts) + coef[1]*(max_wknd_shifts - min_wknd_shifts) + coef[2]*sum(shift_interval_violations)
-    model.minimize(obj_func)
+    for day in range(1, num_days + 1):
+        model.add(sum(decision_vars[id][day] for id in employee_data) == 2)
+    
+    model.add_min_equality(min_shifts, employee_shift_count.values())
+    model.add_max_equality(max_shifts, employee_shift_count.values())
+    model.add_min_equality(min_wknd_shifts, employee_wknd_shift_count.values())
+    model.add_max_equality(max_wknd_shifts, employee_wknd_shift_count.values())
+    
+    obj_1 = max_shifts - min_shifts
+    obj_2 = max_wknd_shifts - min_wknd_shifts
+    obj_3 = sum(shift_interval_violations)
+
     solver = cp_model.CpSolver()
-    solver.parameters.max_time_in_seconds = 3
+    solver.parameters.max_time_in_seconds = 5
+    
+    model.minimize(obj_1)
+    solver.solve(model)
+    model.add(obj_1 == int(solver.objective_value))
+
+    model.minimize(obj_2)
+    solver.solve(model)
+    model.add(obj_2 == int(solver.objective_value))
+
+    model.minimize(obj_3)
+    solver.solve(model)
+    
     status = solver.Solve(model)
 
     if status not in [cp_model.OPTIMAL, cp_model.FEASIBLE]:
         return None
     
     schedule = {}
-    employee_stats = {}   
+    employee_stats = {}
 
     for day in range(1, num_days + 1):
         employees_by_names = [employee_data[e]['full_name'] + f' (id: {e})' for e in employee_data if solver.value(decision_vars[e][day]) == 1]
@@ -103,20 +109,19 @@ def create_schedule(schedule_period: datetime.date, employee_data: dict):
             if day in weekends:
                 employee_stats[employee_name]['weekends'] += 1
     
-    interval_violation_stats = {'total_violations': 0, 'intervals_violated': []}
+    interval_violations = []
     for i in range(len(shift_interval_violations)):
         if solver.value(shift_interval_violations[i]) == 1:
-            interval_violation_stats['total_violations'] += 1
-            interval_violation_stats['intervals_violated'].append(shift_interval_violations[i].Name())
+            interval_violations.append(shift_interval_violations[i].Name())
 
     
     schedule_stats = {
-        'max_shifts - min_shifts': solver.value(max_shifts) - solver.value(min_shifts),
-        'max_wknd_shifts - min_wknd_shifts': solver.value(max_wknd_shifts) - solver.value(min_wknd_shifts),
+        'max_shifts_min_shifts': solver.value(obj_1),
+        'max_wknd_shifts_min_wknd_shifts': solver.value(obj_2),
         'obj_function_value': solver.ObjectiveValue(),
         'status': solver.StatusName(),
         'theoretical_intervals': solver.value(shift_interval),
-        'interval_violations_stats': interval_violation_stats
+        'interval_violations': interval_violations
 
     }
             
